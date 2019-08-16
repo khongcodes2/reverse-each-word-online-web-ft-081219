46858cc46cc40c4174cfbec7c17b581e2971e54a
K Hong <albtrossfacefall@gmail.com>
2019-08-16 10:18:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array=sentence.split(" ")
   new_array=[]
   array.each do |word|
-    word=word.reverse
+    new_array.push(word.reverse)
   end
   array.join(" ")
 end
